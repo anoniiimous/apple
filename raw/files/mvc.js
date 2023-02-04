b92e10d6f9b259bc3c6380181dbebeac2a7b7cb6
thebanon <91961816+thebanon@users.noreply.github.com>
2023-02-03 23:53:03 -0500

    Restore Backup

---
+++ raw/files/mvc.js
@@ -0,0 +1,90 @@
+window.mvc ? null : (window.mvc = {});
+
+window.mvc.m ? null : (window.mvc.m = model = {
+    error: {
+        image: e=>{
+            console.log('model.error.image', e);
+            e.remove();
+        }
+    }
+});
+
+window.mvc.v ? null : (window.mvc.v = view = function(route) {
+    return new Promise(async function(resolve, reject) {
+        var page = route.page;
+        var path = route.path;
+        var gut = route.hash ? rout.ed.dir(route.hash.split('#')[1]) : [];
+        var get = (route ? route.GOT : rout.ed.dir(dom.body.dataset.path)).concat(gut);
+        var root = get[0] || gut[0];
+
+        window.GET = window.GET ? GET : rout.ed.dir(dom.body.dataset.path);
+
+        $(dom.body.all('aside')).remove()
+
+        var page = route.page;
+        var vp = dom.body.find('[data-page="' + page + '"]');
+
+        console.log(108, {
+            route,
+            vp
+        });
+
+        var feeds = vp.all('[data-media]');
+        if (feeds.length > 0) {
+            var f = 0;
+            do {
+                var feed = feeds[f];
+                var limit = feed.dataset.limit;
+                var media = feed.dataset.media;
+                if (media === "pages") {
+                    var json = await ajax('raw/posts/posts.json')
+                    var posts = JSON.parse(json);
+
+                    if (posts.length > 0) {
+
+                        var p = 0;
+                        var html = "";
+
+                        do {
+
+                            var post = posts[p];
+                            var elem = feed.nextElementSibling.content.firstElementChild.cloneNode(true);
+
+                            var date = elem.find('[placeholder="Date"]');
+                            var title = elem.find('[placeholder="Title"]');
+                            var description = elem.find('[placeholder="Description"]');
+                            var picture = elem.find('picture');
+
+                            date.textContent = post.date ? date.textContent = post.date : date.remove();
+
+                            title.textContent = post.title;
+                            description.textContent = post.description;
+                            post.image ? picture.find('img').dataset.src = post.image : null;
+
+                            html += elem.outerHTML;
+
+                            p++;
+
+                        } while (p < limit);
+
+                        feed.innerHTML = html;
+
+                    }
+
+                    console.log(37, {
+                        feed,
+                        media,
+                        posts
+                    });
+                }
+                f++;
+            } while (f < feeds.length)
+        }
+
+        resolve(route);
+    }
+    );
+}
+);
+
+window.mvc.c ? null : (window.mvc.c = controller = {});
